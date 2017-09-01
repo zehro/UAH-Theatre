36e3cab8b26132a5d6c5e3bfa7c3775fe6d32e43
koerino <koerino@gmail.com>
2017-09-01 19:27:50 -0400

    reorganized app to use better architecture

--- uah/views.py
+++ uah/views.py
@@ -1,7 +1,33 @@
-from flask import render_template, Response, json, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from uah import app
 
 @app.route('/')
-@app.route('/<path:path>')
-def index(path=None):
-    return render_template('index.html')
+@app.route('/<path:path>') # all invalid urls will be redirected to index page
+def index_page(path=None):
+    # to be implemented: check auth, if user is authenticated, render home page. Otherwise, render login page.
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        # auth; to be implemented using flask-login
+        return redirect(url_for('home_page'))
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for('index_page'))
+
+@app.route('/user/new', methods=['GET'])
+def register_page():
+    return render_template('register.html')
+
+@app.route('/users', methods=['POST'])
+def register():
+    # add new user; to be implemented
+    return redirect(url_for('login'))
+
+@app.route('/home', methods=['GET'])
+def home_page():
+    return render_template('home.html')
