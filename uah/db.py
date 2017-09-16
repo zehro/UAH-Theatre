fffd80611a68dcd7847482113226a2aeacacf22f
KSholl <kevin74655@gmail.com>
2017-09-16 18:02:18 -0400

    Makes the code valid python lol

--- uah/db.py
+++ uah/db.py
@@ -35,11 +35,11 @@ User.insert          = 'INSERT INTO USER(USERNAME, PASSWORD) VALUES (%(Username)
 User.findby_username = 'SELECT USERNAME, ISADMIN FROM USER WHERE USERNAME = %(Username)s AND ISVERIFIED = 1'
 User.check_login     = 'SELECT USERNAME, ISADMIN FROM USER WHERE USERNAME = %(Username)s AND PASSWORD = %(Password)s AND ISVERIFIED = 1'
 
-def String buildSearch(name, type, condition, era, checkedout, color, dimension, size):
+def String buildSearch(name, objecttype, condition, era, checkedout, color, dimension, size):
 	query = 'SELECT OID FROM OBJECT WHERE';
 	if (name != null):
 		query += ' NAME = %' + str(name) + '% AND '
-	if (type != null):
+	if (objecttype != null):
 		query += ' TYPE = \'' + str(type) + '\''
 	if (condition != null):
 		query += ' OID IN (SELECT OID FROM OBJECT NATURAL JOIN CNDTN WHERE CNDTNNAME = ' + str(condition) + ') AND '
@@ -48,12 +48,12 @@ def String buildSearch(name, type, condition, era, checkedout, color, dimension,
 	if (checkedout != null):
 		if (checkedout):
 			query += ' CHECKEDOUTTO IS NOT NULL AND '
-		else if (!checkedout):
+		else if (not checkedout):
 			query += ' CHECKEDOUTTO IS NULL AND '
 	if (color != null):
 		query += ' OID IN (SELECT OID FROM OBJECTCOLOR NATURAL JOIN COLOR WHERE COLORNAME = ' + str(color) + ') AND '
-	if (type == 'p' && dimension != null):
+	if (objecttype == 'p' and dimension != null):
 		query += ' OID IN (SELECT OID FROM PROP WHERE DIMENSION = ' + str(dimension) + ') AND '
-	if (type == 'c' && size != null):
+	if (objecttype == 'c' and size != null):
 		query += ' OID IN (SELECT OID FROM COSTUME WHERE SIZE = \'' + str(size) + '\' AND '
 	return query[:-5];
